29a852f1fb7ff148586b73057622882da46941db
Josemar Cafumana <rjosemar-cafumana@hotmail.com>
2024-02-05 18:53:44 +0100

    feat: setup

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+</script>
+
+<template>
+  <div class="flex justify-center items-center p-12">
+    <Button> Enviar</Button>
+  </div>
+ 
+</template>
+
